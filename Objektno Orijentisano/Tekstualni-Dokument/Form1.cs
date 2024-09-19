18019a979f9fb33b54c94fd951114bc65985cf91
kripticni <kripitcno@gmail.com>
2024-09-19 13:49:59 +0200

    Dodat prost projekat za prosek iz fajla.

---
+++ Objektno Orijentisano/Tekstualni-Dokument/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Tekstualni_Dokument
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            int brojac = 0, suma = 0;
+            StreamReader _read = new StreamReader("Brojevi.txt");
+            while (!_read.EndOfStream)
+            {
+                suma += Convert.ToInt32(_read.ReadLine());
+                brojac++;
+            }
+            textBox1.Text = Convert.ToString(suma / brojac);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
