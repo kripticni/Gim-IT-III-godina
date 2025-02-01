acbf1fe80c6e516c135f97f12892daa029622d5d
kripticni <kripticno@gmail.com>
2025-02-01 18:12:12 +0100

    figured out this problem

--- Takprog/zbirka/izlog-dva-reda.cpp
+++ Takprog/zbirka/izlog-dva-reda.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <vector>
 #include <tuple>
+#include <queue>
 
 int main() noexcept {
   std::ios::sync_with_stdio(false);
@@ -45,24 +46,57 @@ int main() noexcept {
     return std::get<0>(a) < std::get<0>(b);
   });
 
+  std::priority_queue<uint32_t> min;
   auto it_b = a.begin();
   auto it_a = b.begin();
+  auto beg_a = a.begin();
+  auto beg_b = b.begin();
+  auto temp = b.begin();
   
   while(std::get<0>(*it_a) == std::get<0>(*it_b) &&
         it_a != a.end() && it_b != b.end()){
       ++it_a;
       ++it_b;
   }
-  if(it_a + 1 != a.end())
-    if(std::get<0>(*(it_a + 1)) == std::get<0>(*(it_a))){
-      std::cout << '-';
-      return 0;
+
+  while(std::get<0>(*it_a) == std::get<0>(*(it_a - 1)) && it_a != a.end())
+    ++it_a;
+
+  while(std::get<0>(*it_b) == std::get<0>(*(it_b - 1)) && it_b != b.end())
+    ++it_b;
+
+  std::sort(beg_a, it_a, [](const auto& a, const auto& b){
+    return std::get<1>(a) < std::get<1>(b);
+  });
+  std::sort(beg_b, it_b, [](const auto& a, const auto& b){
+    return std::get<1>(a) < std::get<1>(b);
+  });
+
+  if(it_a - beg_a > it_b - beg_b){
+    while(beg_b != it_b + 1){
+      if( (temp = std::upper_bound(beg_a, it_a, *beg_b)) == it_a+1 ){
+        std::cout << '-';
+        return 0;
+      }
+      --temp;
+      std::swap(beg_a, temp);
+      ++beg_b;
     }
-  if(it_b + 1 != b.end())
-    if(std::get<0>(*(it_b + 1)) == std::get<0>(*(it_b))){
-      std::cout << '-';
-      return 0;
+  } else {
+    while(beg_a != it_a + 1){
+      if( (temp = std::upper_bound(beg_b, it_b, *beg_a)) == it_b+1 ){
+        std::cout << '-';
+        return 0;
+      }
+      --temp;
+      std::swap(beg_b, temp);
+      ++beg_a;
     }
+  }
+
   //separate by segments of same color
+  //it_a shows a segment of same colored books in a row
+  //it_b shows a segment of same colored books in b row
+  //this is much simpler in C, ill do it in C tmrw
 
 }
