04fb1c8a484344a82b2fb71723ac3110fe79bc0c
kripticni <157402936+kripticni@users.noreply.github.com>
2025-03-17 10:54:37 +0100

    Kmp implementacija sa casa

--- Programiranje/string-algoritmi.c
+++ Programiranje/string-algoritmi.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 
+//kmp implementacija sa casa
+//mozete naci ispod main funkcije
+void nizPi(char *p,int m, int *Pi); 
+void KmpSearch(char *p, char *t);
+
 void preproces(char* lps, char str[]) {
     int len = 0; //sufiks sa proslim indeksom
     int i = 1; //krecemo od jedan
@@ -79,4 +84,42 @@ int main(){
 
   kmp(str, text);
   bruteforce(str, text);
+}
+
+
+void nizPi(char *p,int m, int *Pi)
+{
+    Pi[0]=0;
+    int i = 0;
+    int j;
+    for(j=1;j<m;j++)
+    {
+        if(p[j]!=p[i] && i>0)
+            i=Pi[i-1];
+        else {if(p[j]==p[i])
+            i++;
+        Pi[j]=i;}
+    }
+}
+
+void KmpSearch(char *p, char *t)
+{
+    int M=strlen(p);
+    int N=strlen(t);
+    char Pi[20];
+    nizPi(p,M,Pi);
+    int i=0;
+    int j;
+    for(j=0;j<N;j++)
+    {
+        if(t[j]!=p[i] && i>0)
+            i=Pi[i-1];
+        else
+        {
+            if(t[j]==p[i])
+                i++;
+            if(i==M)
+                printf("\n%i",j-M+1);
+        }
+    }
 }
