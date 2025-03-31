70f8e8e15e8c35177db7136a5627e2339a68c6ac
kripticni <157402936+kripticni@users.noreply.github.com>
2025-03-31 11:12:13 +0200

    Gradivo sa casova

--- Programiranje/grafovi.c
+++ Programiranje/grafovi.c
@@ -1,20 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int jeUsmeren(int g[5][5], int n){
+int neusmeren(int g[5][5], int n){
   int i, j;
   for(i = 0; i < n; ++i)
     for(j = i + 1; j < n; ++j){
       if(i == j){
         if(g[i][i] == 1)
-          return 1;
+          return 0;
       } else {
         if(g[i][j] != g[j][i])
-          return 1;
+          return 0;
       }
     }
 
-  return 0;
+  return 1;
+}
+
+int maxStepen(int g[5][5], int n){
+    int i,j,br,max=0;
+    for(i = 0; i < n; ++i, br=0)
+        for(j = 0; i < n; ++i){
+            if(g[i][j]==1) ++br;
+            if(br>max) max=br;
+        }
+    return max;
+}
+int elementStepen(int g[5][5], int k, int n){
+    int i, br = 0;
+    for(i = 0; i < n; ++i)
+        if(g[k][i]==1) ++br;
+    return br;
+}
+
+int ulazneElementa(int g[5][5], int k, int n){
+    int i, br = 0;
+    for(i = 0; i < n; ++i)
+        if(g[i][k]==1) ++br;
+    return br;
+}
+
+int povezaniElementi(int g[5][5], int a, int b){
+    if(g[a][b] == 1 && g[b][a] == 1){
+        printf("Elementi su medjusobno povezani.");
+        return 2;
+    }
+    if(g[a][b] == 1){
+        printf("Element A je povezan na element B.");
+        return 1;
+    }
+    if(g[b][a] == 1){
+        printf("Element A je povezan na element B.");
+        return 1;
+    }
+    printf("Elementi nisu povezani.");
+    return 0;
 }
 
 int main(){
@@ -34,12 +74,12 @@ int main(){
     {1,0,0,0,0}
   };
 
-  if(jeUsmeren(matrica1,n))
+  if(neusmeren(matrica1,n))
      printf("Graf je usmeren\n");
   else
    printf("Graf je neusmeren\n");
 
-  if(jeUsmeren(matrica2,n))
+  if(neusmeren(matrica2,n))
      printf("Graf je usmeren\n");
   else
    printf("Graf je neusmeren\n");
