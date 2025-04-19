21a25f049f40f4e7e9d9db0103c00122894864fe
kripticni <kripticno@gmail.com>
2025-04-19 16:49:17 +0200

    Skracena klasa za mrezu tako sto je iskoriscen kod iz klase netcalc

--- Objektno Orijentisano/Projekti/p2pchat/GUI/Class3.cs
+++ Objektno Orijentisano/Projekti/p2pchat/GUI/Class3.cs
@@ -128,42 +128,12 @@ public class Mreza
                     isPublicIPv4 = true;
                 else if (ip.AddressFamily == AddressFamily.InterNetworkV6)
                     isPublicIPv4 = false;
-                //Console.WriteLine(line);
+                //Console.WriteLine(line); // za debug, Project Properties -> Application -> App Type
                 break;
             }
             //Console.WriteLine(line);
         }
         client.Close();
-    }
-
-    static public bool isLowerAddress(IPAddress a, IPAddress b)
-    {
-        byte[] byteArrayA = a.GetAddressBytes();
-        byte[] byteArrayB = b.GetAddressBytes();
-
-        for (int i = 0; i < byteArrayA.Length; ++i)
-        {
-            if (byteArrayA[i] < byteArrayB[i])
-                return true;
-            if (byteArrayA[i] > byteArrayB[i])
-                return false;
-        }
-
-        return false;
-    }
-
-    public static void IncrementAddress(ref IPAddress ip)
-    {
-        byte[] bytes = ip.GetAddressBytes();
-
-        for (int i = bytes.Length - 1; i >= 0; i--)
-        {
-            bytes[i]++;
-            if (bytes[i] != 0)
-                break;
-        }
-
-        ip = new IPAddress(bytes); //updateujemo isti objekat koji smo dobili na ulazu
     }
 
     public IPAddress Subnet { get; private set; }
@@ -185,74 +155,19 @@ public class Mreza
                 if (unicast2.Address == PrivateIP)
                 {
                     PrefixLength = unicast2.PrefixLength;
-                    Subnet = new IPAddress(CreateSubnetMaskV6(PrefixLength));
+                    Subnet = new IPAddress(NetCalc.CreateSubnetMaskV6(PrefixLength));
                 }
         }
-    }
-    public static byte[] CreateSubnetMask(int n, bool isIPv4)
-    {
-        if (isIPv4)
-            return CreateSubnetMaskV4(n);
-        else
-            return CreateSubnetMaskV6(n);
-    }
-
-    public static byte[] CreateSubnetMaskV4(int n)
-    {
-        byte[] bits = new byte[4];
-
-        for (int i = 0; i < n / 8; ++i)
-        {
-            bits[i] = 0xFF;
-        }
-
-        if (n % 8 != 0)
-        {
-            bits[n / 8] = (byte)(0xFF << (8 - (n % 8)));
-        }
-
-        return bits;
-    }
-
-    public static byte[] CreateSubnetMaskV6(int n)
-    {
-        byte[] bits = new byte[16];
-
-        for (int i = 0; i < n / 8; ++i)
-        {
-            bits[i] = 0xFF;
-        }
-
-        if (n % 8 != 0)
-        {
-            bits[n / 8] = (byte)(0xFF << (8 - (n % 8)));
-        }
-
-        return bits;
     }
 
     public IPAddress NetworkPrefix
     {
-        get
-        {
-                byte[] ip = PrivateIP.GetAddressBytes();
-                byte[] mask = Subnet.GetAddressBytes();
-                for (int i = 0; i < ip.Length; ++i) //idemo do ip.Length jer se mozda radi o V6
-                    ip[i] = (byte)(ip[i] & mask[i]);
-                return new IPAddress(ip);
-        }
+        get{ return NetCalc.LowestAddressInNet(PrivateIP, Subnet); }
     }
 
     public IPAddress Broadcast //ustvari je najveca mreza u subnet-u za v6
     {
-        get
-        {
-            byte[] ip = PrivateIP.GetAddressBytes();
-            byte[] mask = Subnet.GetAddressBytes();
-            for (int i = 0; i < ip.Length; ++i) //idemo do ip.Length jer se mozda radi o V6
-                ip[i] = (byte)(ip[i] | (~mask[i]));
-            return new IPAddress(ip);
-        }
+        get{ return NetCalc.HighestAddressInNet(PrivateIP, Subnet); }
     }
 
     //ip verzija od gateway isto zavisi od lokalne ip verzije
