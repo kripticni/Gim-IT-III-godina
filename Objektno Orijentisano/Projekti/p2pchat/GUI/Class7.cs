3cb745aea5fefe79fde5ec6c6b6e994e18f953e2
kripticni <kripticno@gmail.com>
2025-04-19 16:41:57 +0200

    Pair klasa u C#

---
+++ Objektno Orijentisano/Projekti/p2pchat/GUI/Class7.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Pair<F,S>
+{
+    public F first { get; set; }
+    public S second { get; set; }
+
+    public Pair(F _first, S _second)
+    {
+        first = _first;
+        second = _second;
+    }
+}
