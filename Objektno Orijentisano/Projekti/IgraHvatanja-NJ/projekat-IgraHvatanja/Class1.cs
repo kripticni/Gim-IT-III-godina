53b9c0e021c110a22ae0e30d76b744a0521c3c35
kripticni <kripticno@gmail.com>
2025-04-23 01:58:08 +0200

    Igra hvatanja od NJ

---
+++ Objektno Orijentisano/Projekti/IgraHvatanja-NJ/projekat-IgraHvatanja/Class1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projekat_IgraHvatanja
+{
+    public abstract class Predmet
+    {
+        protected int x, y;
+        protected int velicina;
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        public int Velicina { get; } = 30;
+
+        public Predmet(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void Pomeri()
+        {
+            Y += 5;
+        }
+
+        public bool SudarSa(int korpaX, int korpaY, int korpaSirina, int korpaVisina)
+        {
+            if (korpaX + korpaSirina < X || korpaX > X + Velicina || korpaY + korpaVisina < Y || korpaY > Y + Velicina)
+            { 
+                return false;
+            }
+        return true;
+        }
+        public abstract void Crtaj(Graphics g);
+    }
+    public class Jabuka : Predmet
+    {
+        public Jabuka(int x, int y) : base(x, y) { }
+
+        public override void Crtaj(Graphics g)
+        {
+            SolidBrush cetka1 = new SolidBrush(Color.Red);
+            SolidBrush cetka2 = new SolidBrush(Color.Green);
+            Pen olovka = new Pen(Color.Black, 1);
+            g.FillEllipse(cetka1, X, Y, Velicina, Velicina);
+            g.DrawEllipse(olovka, X, Y, Velicina, Velicina);
+            Point[] listic = {
+            new Point(X + Velicina / 2, Y),
+            new Point(X + Velicina / 2 - 5, Y - 10),
+            new Point(X + Velicina / 2 + 5, Y - 10)
+        };
+            g.FillPolygon(cetka2, listic);
+        }
+    }
+    public class Bomba : Predmet
+    {
+        public Bomba(int x, int y) : base(x, y) { }
+
+        public override void Crtaj(Graphics g)
+        {
+            SolidBrush cetka = new SolidBrush(Color.Black);
+            Pen olovka = new Pen(Color.Orange, 4);
+            g.FillEllipse(cetka, X, Y, Velicina, Velicina);
+            g.DrawLine(olovka, X + Velicina / 2, Y, X + Velicina / 2, Y - 10);
+        }
+    }
+    public class Pomorandza : Predmet
+    {
+        public Pomorandza(int x, int y) : base(x, y) { }
+
+        public override void Crtaj(Graphics g)
+        {
+            SolidBrush cetka1 = new SolidBrush(Color.Orange);
+            SolidBrush cetka2 = new SolidBrush(Color.Green);
+            Pen olovka = new Pen(Color.Black, 1);
+            g.FillEllipse(cetka1, X, Y, Velicina, Velicina);
+            g.DrawEllipse(olovka, X, Y, Velicina, Velicina);
+            Point[] listic = {
+            new Point(X + Velicina / 2, Y),
+            new Point(X + Velicina / 2 - 5, Y - 10),
+            new Point(X + Velicina / 2 + 5, Y - 10)
+        };
+            g.FillPolygon(cetka2, listic);
+        }
+    }
+}
---
+++ Objektno Orijentisano/Projekti/IgraHvatanja-NJ/projekat-IgraHvatanja/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projekat_IgraHvatanja
+{
+    public partial class Form1 : Form
+    {
+        Random r = new Random();
+        List<Predmet> predmeti = new List<Predmet>();
+        Timer t = new Timer();
+
+        int korpaX;
+        int korpaY;
+        int korpaSirina;
+        int korpaVisina = 20;
+        bool gameStarted = false;
+
+        int poeni = 0;
+        int zivoti = 3;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+            korpaY = this.ClientSize.Height - korpaVisina - 10;
+            korpaX = this.ClientSize.Width / 2 - korpaSirina / 2;
+            t.Tick += timer1_Tick;
+            t.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (gameStarted)
+            {
+                if (r.Next(0, 10) < 2)
+                    DodajPredmet();
+                for (int i = predmeti.Count - 1; i >= 0; i--)
+                {
+                    predmeti[i].Pomeri();
+
+                    if (predmeti[i].SudarSa(korpaX, korpaY, korpaSirina, korpaVisina))
+                    {
+                        if (predmeti[i] is Jabuka || predmeti[i] is Pomorandza)
+                            poeni++;
+                        else if (predmeti[i] is Bomba)
+                            zivoti--;
+
+                        predmeti.RemoveAt(i);
+                    }
+                    else if (predmeti[i].Y > this.ClientSize.Height)
+                    {
+                        predmeti.RemoveAt(i);
+                    }
+                }
+
+                Refresh();
+
+                if (zivoti <= 0)
+                {
+                    t.Stop();
+                    MessageBox.Show("Kraj igre! Poeni: " + poeni);
+                    Close();
+                }
+            }
+        }
+
+        private void DodajPredmet()
+        {
+            int x = r.Next(0, this.ClientSize.Width - 30);
+            int tip = r.Next(0, 3);
+            Predmet p;
+
+            if (tip == 0)
+                p = new Jabuka(x, 0);
+            else if (tip == 1)
+                p = new Bomba(x, 0);
+            else
+                p = new Pomorandza(x, 0);
+
+            predmeti.Add(p);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+            button2.Visible = false;
+            radioButton1.Visible = false;
+            radioButton2.Visible = false;
+            radioButton3.Visible = false;
+            label1.Visible = false;
+            label2.Visible = false;
+            gameStarted = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (gameStarted)
+            {
+                Graphics g = e.Graphics;
+                SolidBrush cetka1 = new SolidBrush(Color.SaddleBrown);
+                SolidBrush cetka2 = new SolidBrush(BackColor);
+                Pen olovka = new Pen(Color.Black, 1);
+                Rectangle korpaRect = new Rectangle(korpaX, korpaY, korpaSirina, korpaVisina);
+                g.FillEllipse(Brushes.SaddleBrown, korpaRect);
+                g.DrawEllipse(Pens.Black, korpaRect);
+                Rectangle gornjiDeo = new Rectangle(korpaX, korpaY, korpaSirina, korpaVisina / 2);
+                g.FillRectangle(cetka2, gornjiDeo);
+                for (int i = 0; i < predmeti.Count; i++)
+                {
+                    predmeti[i].Crtaj(e.Graphics);
+                }
+                label1.Visible = true;
+                label2.Visible = true;
+                label1.Text = "Poeni: " + poeni;
+                label2.Text = "Životi: " + zivoti;
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            t.Interval = 75;
+            korpaSirina = 150;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            t.Interval = 30;
+            korpaSirina = 80;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            t.Interval = 1;
+            korpaSirina = 40;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            korpaX = e.X - 25;
+            korpaY = this.ClientSize.Height - 50;
+            Refresh();
+        }
+    }
+}
