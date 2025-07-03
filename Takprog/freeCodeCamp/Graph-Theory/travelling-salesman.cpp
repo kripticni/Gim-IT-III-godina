5d1ad0513dcc44fed6324593c374455e3b0ab075
kripticni <kripticno@gmail.com>
2025-07-03 23:37:08 +0200

    freecodecamp graph theory travelling-salesman

---
+++ Takprog/freeCodeCamp/Graph-Theory/travelling-salesman.cpp
@@ -0,0 +1,14 @@
+#include <cstdint>
+#include <iostream>
+#include <stack>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+typedef unordered_map<uint16_t, unordered_map<uint16_t, int16_t>> Graph;
+constexpr int16_t INF = 30000;
+constexpr int16_t NEG_INF = -30000;
+constexpr int16_t unvisited = -1;
+
+int main() noexcept { Graph graph; }
